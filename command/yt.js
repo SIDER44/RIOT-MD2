b732f093fc6992c3a27589ad2fab9f8176a8a45f
SIDER44 <siderchao837@gmail.com>
2026-02-19 19:32:09 +0300

    Delete command/yt.js

--- command/yt.js
+++
@@ -1,6 +0,0 @@
-export const name = 'yt'
-export async function run(sock, msg, args) {
-  const url = args[0]
-  if (!url) return await sock.sendMessage(msg.key.remoteJid, { text: 'Send a YouTube link!' })
-  await sock.sendMessage(msg.key.remoteJid, { text: `Downloading from YouTube: ${url}` })
-}
